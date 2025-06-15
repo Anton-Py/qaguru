fbc27b5256db68ca0ebfc0e0eea77d36d8c4da42
anton.lyakh <anton.lyakh@vk.team>
2025-06-15 20:15:10 +0700

    add homework 6

---
+++ homework_6_python_part_2/test_homework.py
@@ -0,0 +1,127 @@
+from datetime import time
+
+
+def test_dark_theme_by_time():
+    """
+    Протестируйте правильность переключения темной темы на сайте в зависимости от времени
+    """
+    current_time = time(hour=21)
+
+    # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
+
+    if current_time >= time(hour=22) or current_time <= time(hour=6):
+        is_dark_theme = True
+    else:
+        is_dark_theme = False
+
+    assert is_dark_theme is True
+
+
+def test_dark_theme_by_time_and_user_choice():
+    """
+    Протестируйте правильность переключения темной темы на сайте
+    в зависимости от времени и выбора пользователя
+    dark_theme_enabled_by_user = True - Темная тема включена
+    dark_theme_enabled_by_user = False - Темная тема выключена
+    dark_theme_enabled_by_user = None - Пользователь не сделал выбор (используется переключение по времени системы)
+    """
+    current_time = time(hour=22)
+    dark_theme_enabled_by_user = True
+    # TODO переключите темную тему в зависимости от времени суток,
+    #  но учтите что темная тема может быть включена вручную
+
+    if dark_theme_enabled_by_user is not None:
+        if current_time >= time(hour=22) or current_time <= time(hour=6):
+            is_dark_theme = True
+        else:
+            is_dark_theme = False
+    elif dark_theme_enabled_by_user:
+        is_dark_theme = True
+    else:
+        is_dark_theme = False
+
+    assert is_dark_theme is True
+
+
+def test_find_suitable_user():
+    """
+    Найдите нужного пользователя по условиям в списке пользователей
+    """
+    users = [
+        {"name": "Oleg", "age": 32},
+        {"name": "Sergey", "age": 24},
+        {"name": "Stanislav", "age": 15},
+        {"name": "Olga", "age": 45},
+        {"name": "Maria", "age": 18},
+    ]
+
+    # TODO найдите пользователя с именем "Olga"
+    suitable_users = None
+
+    for key_value in users:
+        if key_value["name"] == "Olga":
+            suitable_users = key_value
+
+    assert suitable_users == {"name": "Olga", "age": 45}
+
+    # TODO найдите всех пользователей младше 20 лет
+    suitable_users = None
+
+    if suitable_users is None:
+        suitable_users = []
+
+        for element in users:
+            if element["age"] < 20:
+                suitable_users.append(element)
+
+    assert suitable_users == [
+        {"name": "Stanislav", "age": 15},
+        {"name": "Maria", "age": 18},
+    ]
+
+
+# Сделайте функцию, которая будет печатать
+# читаемое имя переданной ей функции и значений аргументов.
+# Вызовите ее внутри функций, описанных ниже
+# Подсказка: Имя функции можно получить с помощью func.__name__
+# Например, вызов следующей функции должен преобразовать имя функции
+# в более читаемый вариант (заменить символ подчеркивания на пробел,
+# сделать буквы заглавными (или первую букву), затем вывести значения всех аргументов этой функции:
+# >>> open_browser(browser_name="Chrome")
+# "Open Browser [Chrome]"
+
+def test_custom_function(name_func, *argument):
+    functions_name = name_func.__name__.replace("_", " ").split()
+    list_of_words = list()
+
+    args_name = ', '.join([*argument])
+
+    for f in functions_name:
+        word = f[:1].upper() + f[1:]
+        list_of_words.append(word)
+
+    result_string = " ".join(list_of_words) + f' [{args_name}]'
+
+    print(result_string)
+    return result_string
+
+
+def test_readable_function():
+    open_browser(browser_name="Chrome")
+    go_to_companyname_homepage(page_url="https://companyname.com")
+    find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
+
+
+def open_browser(browser_name):
+    actual_result = test_custom_function(open_browser, browser_name)
+    assert actual_result == "Open Browser [Chrome]"
+
+
+def go_to_companyname_homepage(page_url):
+    actual_result = test_custom_function(go_to_companyname_homepage, page_url)
+    assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
+
+
+def find_registration_button_on_login_page(page_url, button_text):
+    actual_result = test_custom_function(find_registration_button_on_login_page, page_url, button_text)
+    assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
